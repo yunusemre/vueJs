316335b8832c9d79909a5e444d757f63f4a6583f
yunusemretatar <yunusemre.tatar@sekomyazilim.com.tr>
2017-03-03 17:42:06 +0300

    new project

---
+++ src/components/Preview.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="preview">
+    <ul>
+      <li v-for="gif in gifs">
+        <img :src="gif.images.fixed_height.url" alt="gif.images.fixed_height.name">
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+	export default {
+		props: ['gifs']
+	}
+</script>
+
+<style>
+  ul {
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    padding: 0 10px 10px 0;
+  }
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="search">
+		<input placeholder="Hüüüloooooğğğğğğğğ" type="text" v-model="query" @keypress.enter="handleSearch"/>
+		<button  @click="handleSearch">Search</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+				query: 'patron çıldırdı'
+			}
+		},
+		methods: {
+			handleSearch(){
+				this.$emit('SearchRequested', this.query);
+			}
+		}
+	}
+</script>
+
+
+<style>
+	.search input {
+		padding: 10px;
+		border-radius: 4px;
+		border: 1px solid #ddd;
+		width: 300px;
+	}
+
+  .search input:focus, .search input:active {
+    border: 1px solid #d9edf7;
+	}
+
+	.search button {
+		padding: 8px;
+    background: #fff;
+    border: 0;
+    border-radius: 4px;
+    color: #666;
+    font-size: 14px;
+    border: 1px solid #ddd;
+	}
+
+</style>
